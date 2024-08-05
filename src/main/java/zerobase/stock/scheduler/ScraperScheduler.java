a661d270f4b7a30d861c884f6efa9fc44c5ae941
kim <scilo200@gmail.com>
2024-08-05 18:08:05 +0900

    스케줄러 개발

---
+++ src/main/java/zerobase/stock/scheduler/ScraperScheduler.java
@@ -0,0 +1,63 @@
+package zerobase.stock.scheduler;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import zerobase.stock.model.Company;
+import zerobase.stock.model.ScrapeResult;
+import zerobase.stock.persist.CompanyRepository;
+import zerobase.stock.persist.DividendRepository;
+import zerobase.stock.persist.entity.CompanyEntity;
+import zerobase.stock.persist.entity.DividendEntity;
+import zerobase.stock.scraper.Scraper;
+
+import java.util.List;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ScraperScheduler {
+
+    private final CompanyRepository companyRepository;
+    private final DividendRepository dividendRepository;
+    private final Scraper yahooFinanceScraper;
+
+    @Scheduled(cron = "${scheduler.scrap.yahoo}")
+    public void yahooFinanceScheduling() {
+        log.info("Starting scheduler is starting");
+        // 저장된 회사 목록을 조회
+        List<CompanyEntity> companyEntityList = companyRepository.findAll();
+
+        // 회사 맘다 배당금 정보를 새로 스크래핑
+        for (CompanyEntity companyEntity : companyEntityList) {
+
+            ScrapeResult scrapeResult = yahooFinanceScraper.scrap(
+                    Company.builder()
+                    .name(companyEntity.getName())
+                    .ticker(companyEntity.getTicker())
+                    .build()
+            );
+
+            // 스크래핑한 배당금 정보 중 데이터베이스에 없는 값은 저장
+            scrapeResult.getDividendEntities().stream()
+                    .map(e -> new DividendEntity(companyEntity.getId(), e))
+                    .forEach(e -> {
+                        boolean exists = dividendRepository.existsByCompanyIdAndDate(e.getCompanyId(), e.getDate());
+                        if (!exists) {
+                            dividendRepository.save(e);
+                        }
+                    });
+
+            //연속적으로 스크래핑 대상 사이토 서버에 요청을 날리지 않도록 일시정지
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+
+
+    }
+
+}
